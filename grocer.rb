1935cb871d8dacc79aeab5955be7db6cb8cfb12e
Saif Jamal <saifjamal4649@gmail.com>
2020-02-17 00:42:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -69,5 +69,5 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
-  final_cart = 
+  final_cart = apply_clearance
 end
